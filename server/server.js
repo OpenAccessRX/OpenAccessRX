b04464c0b511ea52bf073fe2d2689e5643acb302
El Brewster <167570227+elbi3@users.noreply.github.com>
2025-11-26 10:48:38 -0800

    Add essential Express routes and adjust route in LoginHTML form action

--- server/server.js
+++ server/server.js
@@ -2,17 +2,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import testRoute from "./routes/test.js";
+import authRoute from "./routes/auth.js";
+import chatRoute from "./routes/chat.js";
 import ragRoute from "./routes/rag.js";
+import testRoute from "./routes/test.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/test-db", testRoute); //keep to test db connection
+app.use("/api/auth", authRoute);
+app.use("/api/chat", chatRoute);
 app.use("/api/rag", ragRoute);
+app.use("/api/test-db", testRoute); //keep to test db connection
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`API running on port ${PORT}`));
--- src/components/ChatWindow.svelte
+++ src/components/ChatWindow.svelte
@@ -1,8 +1,36 @@
 <script>
     const username = "Seattle Frog"; //to dynamically render logged in user's name
+    let messages = [];
+    let input = "";
 
+    async function sendMessage() {
+        messages.push({sender: "user", text: input });
+
+        const res = await fetch("/api/chat", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ message: input })
+        })
+        
+        const data = await res.json();
+        messages.push({ sender: "bot", text: data.reply })
+
+        input = "";
+    }
+
+    async function resetChat() {
+        await fetch("/api/chat/reset", { method: "POST" });
+        messages = [];
+    }
 </script>
 
-<section>
-    
+<section class="chatterbox">
+    <div id="message-display" class="messages">
+        {#each messages as msg}
+            <div>{msg.sender}: {msg.text}</div>
+        {/each}
+    </div>
+    <input type="text" id="chat-window" name="" required size="50">
+    <button class="chat-button" on:click={sendMessage}>Send</button>
+    <button class="chat-button" on:click={resetChat}>Reset</button>
 </section>
